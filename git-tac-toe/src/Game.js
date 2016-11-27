c4632f7c5968886ad4f2a6532946e1ff794946dd
Danny Tunney <dtun@me.com>
2016-11-26 19:45:22 -0700

    Restart game functionality

--- git-tac-toe/src/Game.js
+++ git-tac-toe/src/Game.js
@@ -80,6 +80,7 @@ class Game extends React.Component {
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
         </div>
+        <a onClick={() => location.reload()}>Restart</a>
       </div>
     );
   }
